2c69044bc834b21b803cffd7341a9e2f94db72f9
hatatat-dev <hatatat@glowstonemania.com>
2024-09-28 09:50:54 -0700

    [settings] use slot 7

--- src/main.py
+++ src/main.py
@@ -23,36 +23,7 @@ def autonomous_function():
 
     log(("Competition", "competition"), "autonomous_begin")
 
-    # Reset odometry to the starting autonomous position
-    odometry.reset(PositionWithHeading(-1500, 600, -90))
-
-    # Then try resetting it to GPS if GPS sensor is installed and reports high quality
-    reset_odometry_to_gps()
-
-    intake_1st_stage.set_velocity(470, RPM)
-    intake_2nd_stage.set_velocity(470, RPM)
-    pid_driver.drive(-1000, True)
-    clamp.set(True)
-    pid_turner.turn(80, FRAME_HEADING_RELATIVE)
-
-    intake_1st_stage.spin(REVERSE)
-    intake_2nd_stage.spin(FORWARD)
-
-    wait(1000, MSEC)
-    reset_odometry_to_gps()
-
-    pid_driver.drive(550)
-    pid_turner.turn(60, FRAME_HEADING_RELATIVE)
-    pid_driver.drive(270)
-
-    wait(1000, MSEC)
-    reset_odometry_to_gps()
-
-    pid_turner.turn(-80, FRAME_HEADING_RELATIVE)
-    pid_driver.drive(-600, False)
-
-    wait(1000, MSEC)
-    reset_odometry_to_gps()
+    # Add autonomous logic here
 
     log(("Competition", "competition"), "autonomous_end")
 
